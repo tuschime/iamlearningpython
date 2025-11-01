0da05f9cb235dad8521e75946cab251446a959e6
tuschime <tuschime@gmail.com>
2025-11-01 14:57:20 +0300

    Made some changes

---
+++ c#/Program.cs
@@ -0,0 +1,7 @@
+Console.Title = "C# Project";
+Console.WriteLine("Hello, World!");
+Console.WriteLine("What is your name?");
+Console.ReadLine();
+Console.WriteLine("Oh, pretty interesting name!");
+Console.WriteLine("Press any key to EXIT");
+Console.ReadKey();
---
+++ ders.py
@@ -0,0 +1,9 @@
+isim = "Yusuf" # String-Metin
+yas = 15 #Integer-Tam Sayı
+okul = "Konya Anadolu İmam Hatip Lisesi" # Bu da String-Metin
+boy = 1.78 # Float-Ondalıklı Sayı
+takim = "Galatasaray"
+ogrenci_miyim = True # Boolean-Mantıklsal Değer True/False
+
+print("Merhabalar!Ben ", isim, yas, "yasindayim ve", okul, "okulunda okuyorum.Ayrıca", takim, "takımını tutuyorum.") #HEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHE
+print("İsim türü:", type(isim), "Yaş türü:", type(yas), "Okul türü:", type(okul), "Boy türü:", type(boy), "Takım türü:", type(takim), "Öğrenci türü:", type(ogrenci_miyim))
