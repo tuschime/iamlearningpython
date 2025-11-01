e0fed374dc554b570751cdfb897a826fcca2bba6
tuschime <tuschime@gmail.com>
2025-11-01 15:14:52 +0300

    İşlem yapmayı öğrendim.

--- ders.py
+++ ders.py
@@ -5,5 +5,5 @@ boy = 1.78 # Float-Ondalıklı Sayı
 takim = "Galatasaray"
 ogrenci_miyim = True # Boolean-Mantıklsal Değer True/False
 
-print("Merhabalar!Ben ", isim, yas, "yasindayim ve", okul, "okulunda okuyorum.Ayrıca", takim, "takımını tutuyorum.") #HEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHE
-print("İsim türü:", type(isim), "Yaş türü:", type(yas), "Okul türü:", type(okul), "Boy türü:", type(boy), "Takım türü:", type(takim), "Öğrenci türü:", type(ogrenci_miyim))
+print("Merhabalar!Ben ", isim, yas, "yasindayim ve", okul, "okulunda okuyorum.Ayrica", takim, "takimini tutuyorum.") #HEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHEHE
+print("İsim türü:", type(isim), "Yaş türü:", type(yas), "Okul türü:", type(okul), "Boy türü:", type(boy), "Takim türü:", type(takim), "Öğrenci türü:", type(ogrenci_miyim))
---
+++ ders02.py
@@ -0,0 +1,11 @@
+a = 10
+b = 3
+
+print("So-uç-laar:")
+print("Toplam:", a+b)
+print("Fark", a-b)
+print("Çarpım:", a*b)
+print("Bölüm", a/b)
+print("Farksız/Modsuz Bölüm:", a//b)
+print("Kalan (Mod)",a%b)
+print("Bu kadaaaar") #Hayattan nefret ediyorum
